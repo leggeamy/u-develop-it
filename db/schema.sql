36ea9fb697a60c0260ffe72b9c55408393a213d1
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-11-22 14:57:25 -0500

    create GET routes for Voters

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE voters (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL
+    email VARCHAR(50) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
     );
 
